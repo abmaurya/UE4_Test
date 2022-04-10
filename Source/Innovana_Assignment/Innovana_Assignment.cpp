962bfdca5f84e4a063b1ae1a8f231ab32743978b
crocdemon <abhinavmaurya3@gmail.com>
2022-04-10 16:34:43 +0530

    Add CPP sources to project

---
+++ Source/Innovana_Assignment/Innovana_Assignment.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Innovana_Assignment.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Innovana_Assignment, "Innovana_Assignment" );
